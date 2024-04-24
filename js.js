4ef67e769479d64f8e1e17001f1786aff28b9ffb
H R ABHINANDAN <96574240+hrabhinandan@users.noreply.github.com>
2024-04-25 01:45:01 +0530

    my added
    hai

---
+++ js.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    strings: ["Java Full Stack Developer","Web Developer","Video Editor","Google Map Editor"],
+    typeSpeed: 100,
+    backSpeed:100,
+    backDelay: 100,
+    loop: true
+})
